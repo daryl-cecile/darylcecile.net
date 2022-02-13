28580805c3815e9ffbf9ae461707471826de5263
Daryl <darylcecile@gmail.com>
2022-02-13 03:42:38 +0000

    updated linkedin

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -52,7 +52,8 @@ export default function Layout({ children, home, showBackBtn }:LayoutOptions) {
 						/>
 						<h1 className={styles.heroTitle}>{name}</h1>
 						<p className={utilStyles.paragraph}>I'm a Software developer hopping between Nottingham 🎓 and London 🏡. I recently graduated with a BSc in Computer Science at NTU. In between work and travelling (pre-🦠), I spend my time <a href="/projects" hrefLang="en">making things</a> on the tippy tapper. ⌨️</p>
-						<p className={utilStyles.paragraph}>Check out my 2020-2021 <a href="/CV/CV-2021.pdf" rel="noopener" target="_blank">CV</a> (<a href="/CV/CV-2021-ATS.pdf" rel="noopener" target="_blank">ATS</a>)</p>
+						<p className={utilStyles.paragraph}>Check out my <a href="/CV/CV-2021.pdf" rel="noopener" target="_blank">CV</a> (<a href="/CV/CV-2021-ATS.pdf" rel="noopener" target="_blank">ATS</a>) from 2020-2021. <br/> My Career updates are on <a
+							href="https://linkedin.com/in/darylcecile" target={"_blank"} rel={"noopener"}>LinkedIn</a></p>
 					</header>
 					<hr className={utilStyles.push}/>
 				</>
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -19,6 +19,7 @@ export default function ProjectsListingPage({ allProjectsData }: { allProjectsDa
       <div className="restrict">
           <h1 className={utilStyles.header}>Projects</h1>
           <p className={utilStyles.paragraph}>I'm constantly flowing with ideas for tools and projects 🧠; and sometimes, I manage to scrape together the time to work on them ✨. Here are some of the projects that made the cut.</p>
+          <p className={utilStyles.paragraph}>Check out my <a href="https://github.com/daryl-cecile" target={"_blank"} rel={"noopener"}>GitHub</a> for more up-to-date development and proof-of-concepts</p>
 
           <hr className={utilStyles.push}/>
 
