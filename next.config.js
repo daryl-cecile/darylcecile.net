3feb548899dc761dd889480fc221012524f09314
Daryl <darylcecile@gmail.com>
2022-03-01 16:50:34 +0000

    added redirect for externally cached US-Locale links

--- next.config.js
+++ next.config.js
@@ -5,4 +5,18 @@ module.exports = {
 	// 	localeDetection: false,
 	// },
 	poweredByHeader: false,
+	async redirects() {
+		return [
+			{
+				source: '/en-US',
+				destination: '/',
+				permanent: true,
+			},
+			{
+				source: '/en-US/:path*',
+				destination: '/:path*',
+				permanent: true,
+			},
+		]
+	},
 }
