1ffd48206f4b022ea5395d87a6a4bb4436754dce
Daryl <darylcecile@gmail.com>
2022-03-01 16:41:03 +0000

    disabled locale detection

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	i18n: {
-		locales: ['en-GB', 'en-US'],
-		defaultLocale: 'en-GB'
+		localeDetection: false, // disable for now until esp ready for go-live
 	},
 	poweredByHeader: false,
 }
