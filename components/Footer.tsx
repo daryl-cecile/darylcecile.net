99717ff48110bd3cfe333fb706ef6ee5c1a61439
Daryl Cecile <6310278+daryl-cecile@users.noreply.github.com>
2022-05-25 18:30:36 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import {useRef}, React from "react";
 import css from "../styles/footer.module.scss";
 import utilsCss from "../styles/utils.module.scss"
 import SocialLinks from "./SocialLinks";
 
 export default function Footer() {
+	const footerParaRef = useRef();
+	
+	useEffect(()=>{
+		footerRef.current?.innerText = `Daryl Cecile © ${(new Date()).getFullYear()}`;
+	}, []);
+	
 	return (
 		<footer className={css.footer}>
 			<div className={css.footerContent}>
 				<div>
 					<img className={css.footerImage} src="/images/core/profile_full_color_144.png" alt="" aria-hidden="true"/>
 					<div className={css.footerCopy}>
-						<p className={`${css.footerCopyText} ${utilsCss.paragraph}`}>Daryl Cecile © 2021</p>
+						<p ref={footerParaRef} className={`${css.footerCopyText} ${utilsCss.paragraph}`}>Daryl Cecile © {(new Date()).getFullYear()}</p>
 						<SocialLinks/>
 					</div>
 				</div>
