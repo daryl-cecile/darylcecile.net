06ca8bf54dbb98a2a591624c61a871f0e822de4d
Daryl <darylcecile@gmail.com>
2022-06-04 21:47:02 +0100

    new blog post & added splitbee

--- components/Imports.tsx
+++ components/Imports.tsx
@@ -3,10 +3,11 @@ import React from "react";
 export default function Imports() {
 	return (
 		<>
-			<script src="https://kit.fontawesome.com/2355b6b2ba.js" crossOrigin="anonymous"
-	integrity="sha384-Hk1aFY5x9VcMk6YL/QL4mcEbhmtSnlGOPlbh8j75qYmxiRPrhDKPiwRWng8w7aGI" async/>
+	{/*		<script src="https://kit.fontawesome.com/2355b6b2ba.js" crossOrigin="anonymous"*/}
+	{/*integrity="sha384-Hk1aFY5x9VcMk6YL/QL4mcEbhmtSnlGOPlbh8j75qYmxiRPrhDKPiwRWng8w7aGI" async/>*/}
 
 			<script src="/scripts/twemojiWrapper.js" async/>
+			<script async data-api="/_hive" src="/bee.js"/>
 		</>
 	)
 }
--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ module.exports = {
 		localeDetection: false,
 	},
 	poweredByHeader: false,
+	async rewrites() {
+		return [
+			{
+				source: "/bee.js",
+				destination: "https://cdn.splitbee.io/sb.js",
+			},
+			{
+				source: "/_hive/:slug",
+				destination: "https://hive.splitbee.io/:slug",
+			},
+		];
+	},
 	async redirects() {
 		return [
 			{
